1a071872191236b6715bf98988b3bb0ff390b694
anonymousrecords <a.Records2022@gmail.com>
2023-08-29 21:57:52 +0900

    feat : Tab 구현

---
+++ src/Home/components/Tabs.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+interface TabsProps {
+  selectedTool: string;
+  setSelectedTool: (tool: string) => void;
+}
+
+export default function Tabs({ selectedTool, setSelectedTool }: TabsProps) {
+  return (
+    <Tabbar>
+      <TabButton
+        isselected={selectedTool === 'recommend'}
+        onClick={() => setSelectedTool('recommend')}
+      >
+        진영추천
+      </TabButton>
+      <TabButton
+        isselected={selectedTool === 'new'}
+        onClick={() => setSelectedTool('new')}
+      >
+        신상품
+      </TabButton>
+      <TabButton
+        isselected={selectedTool === 'best'}
+        onClick={() => setSelectedTool('best')}
+      >
+        베스트
+      </TabButton>
+    </Tabbar>
+  );
+}
+
+const Tabbar = styled.div`
+  min-width: 375px;
+  max-width: 450px;
+  height: 36px;
+  background: white;
+  display: flex;
+  justify-content: center;
+`;
+
+const TabButton = styled.button<{ isselected: boolean }>`
+  width: 120px;
+  height: 36px;
+  border-bottom: ${(props) =>
+    props.isselected ? '1.5px solid #ff9900' : '1px solid #d9d9d9'};
+  color: ${(props) => (props.isselected ? '#ff9900' : '#D9D9D9')};
+  text-align: center;
+  font-family: Noto Sans;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: ${(props) => (props.isselected ? 700 : 400)};
+  line-height: 140%;
+`;
