b6390d8cfb453bcc54cf1bb6fe6f1e6b353e56b2
anonymousrecords <a.Records2022@gmail.com>
2023-08-30 03:15:23 +0900

    feat : 임시 pagination 구현

--- src/Home/components/RecommendPannel.tsx
+++ src/Home/components/RecommendPannel.tsx
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
-import ItemSwiper from './RecommendPannel/ItemSwiper';
-import Tag from './RecommendPannel/Tag';
-import HotItem from './RecommendPannel/HotItem';
+import styled from "styled-components";
+import ItemSwiper from "./RecommendPannel/ItemSwiper";
+import Tag from "./RecommendPannel/Tag";
+import HotItem from "./RecommendPannel/HotItem";
+import Bullet from "./RecommendPannel/Bullet";
 
 export default function RecommendPannel() {
   return (
@@ -9,18 +10,21 @@ export default function RecommendPannel() {
       <SwiperContainer>
         <ItemSwiper />
       </SwiperContainer>
+      <PaginationContainer>
+        <Bullet />
+      </PaginationContainer>
       <Tag />
       <HotItemContainer>
         <div
           style={{
-            color: '#000',
-            fontFamily: 'Noto Sans',
-            fontSize: '18px',
-            fontStyle: 'normal',
+            color: "#000",
+            fontFamily: "Noto Sans",
+            fontSize: "18px",
+            fontStyle: "normal",
             fontWeight: 600,
-            lineHeight: 'normal',
-            letterSpacing: '0.018px',
-            marginBottom: '13px',
+            lineHeight: "normal",
+            letterSpacing: "0.018px",
+            marginBottom: "13px",
           }}
         >
           블랙프라이데이 인기 상품
@@ -43,11 +47,20 @@ const StyledRecommendPannel = styled.div`
 
 const SwiperContainer = styled.div`
   width: 375px;
-  display: 'flex';
-  padding: 22px;
+  display: "flex";
+  /* padding: 22px; */
   /* justify-content: 'center';
   align-items: 'center'; */
   /* background: yellow; */
+`;
+
+const PaginationContainer = styled.div`
+  width: 375px;
+  height: 33px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background: red; */
 `;
 
 const HotItemContainer = styled.div`
