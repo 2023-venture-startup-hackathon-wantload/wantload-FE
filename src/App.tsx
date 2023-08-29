5bc87b11a9108f420f2ebe1eadc04c93d7cfbead
anonymousrecords <a.Records2022@gmail.com>
2023-08-30 03:21:22 +0900

    feat : swiper 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 // import Puzzle from './components/Puzzle';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
 
 const App: React.FC = () => {
   return (
