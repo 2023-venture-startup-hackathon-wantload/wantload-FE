1e9e0ba530b1e9df3082ba0f16bf8074e0620eb2
anonymousrecords <a.Records2022@gmail.com>
2023-08-29 21:59:03 +0900

    feat : 기타 탭(new, best) 임시 구현

---
+++ src/Home/components/NewPannel.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export default function NewPannel() {
+  return (
+    <StyledNewPannel>
+      <img
+        src="/assets/icons/waiting.svg"
+        style={{ width: '20px', marginBottom: '20px' }}
+      />
+      <Comment>아직 서비스 준비 중입니다.</Comment>
+    </StyledNewPannel>
+  );
+}
+
+const StyledNewPannel = styled.div`
+  min-width: 375px;
+  max-width: 450px;
+  height: 714px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Comment = styled.div`
+  color: #000;
+  font-family: Noto Sans;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: 0.018px;
+`;
