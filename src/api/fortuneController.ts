90c2ef875e7ca88351b7e089060915d614e521f7
Ginieee <kaj1226@naver.com>
2023-08-30 09:04:30 +0900

    fix : ESLint 오류 수정

--- src/api/fortuneController.ts
+++ src/api/fortuneController.ts
@@ -1,8 +1,8 @@
-import { baseAxios } from './baseAxios';
+import { baseAxios } from "./baseAxios";
 
 export const getFortuneItems = async () => {
   const response = await baseAxios
-    .get('/api/fortune')
+    .get("/api/fortune")
     .then((response) => response.data.data);
   console.log(response);
   return response;
